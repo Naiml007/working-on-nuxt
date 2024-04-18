1e7cf76e0d8098a5573074fd5e70fb8e8f0d21ac
Naiml007 <naimurdj9@gmail.com>
2024-04-18 23:04:37 +0600

    first commit

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+    <!-- <div>
+    <NuxtWelcome />
+  </div> -->
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+  <h1>webp</h1>
+  <img src="~/assets/img/img1.webp" alt="Discover Nuxt 3" />
+  <h1>jpg</h1>
+  <img src="~/assets/img/img.jpg" alt="Discover Nuxt 1" />
+</template>
